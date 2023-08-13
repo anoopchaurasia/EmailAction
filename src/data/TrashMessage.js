d13b381dd91d8e21a82154be189c734e8512ca95
Anoop <anoop26667@gmail.com>
2023-08-13 19:27:14 +0530

    removed unncessary code

--- src/data/TrashMessage.js
+++ src/data/TrashMessage.js
@@ -25,34 +25,3 @@ export default TrashMessage = {
 
     }
 }
-
-    // function trashBySender(x) {
-    //     let item = list.find(l => l.sender == x.sender);
-    //     selected[x.sender] = item;
-    //     moveToTrash();
-    // }
-
-        // async function moveToTrash() {
-    //     await commonAction(async sender=> await ProcessRules.createNewRule({to_label:"trash"}, sender, 'trash')).catch(e=>{
-    //         if(e.message == 'rule already exist') return Alert.alert('Rule already exist', "There is already a rule for this sender. Please delete the rule and try again.");
-    //     });
-    // }
-
-    // async function commonAction(action) {
-    //     if (Object.values(selected).length == 0) return console.log("no selection");
-    //     console.log('action', Object.values(selected).map(x => x.sender));
-    //     let senders = Object.values(selected).map(x => x.sender);
-    //     let newlist = list.map(x => x);
-    //     for (let i = 0; i < senders.length; i++) {
-    //         let sender = senders[i];
-    //         await action(sender);
-    //         MessageAggregateService.deleteBySender(sender);
-    //         let index = newlist.indexOf(selected[sender]);
-    //         delete selected[sender];
-    //         console.log(newlist.length);
-    //         index != -1 && newlist.splice(index, 1);
-    //         console.log(index, selected, newlist.length);
-    //     }
-    //     setList(newlist);
-    //     setSelected({});
-    // }
